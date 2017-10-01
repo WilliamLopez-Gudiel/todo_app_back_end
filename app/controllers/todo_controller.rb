4124e9dd94aa2941e52dc57658e138584b79f1ae
William <william.falco.lopez@gmail.com>
2017-10-01 13:36:03 +0000

    updated if statements

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,15 +3,22 @@ class TodoController < ApplicationController
     end
     
     def show
-        todo_id = '2'
+        todo_id = params[:id]
         if todo_id=='1'
                @todo_description ="finish homework"
                @todo_pomodoro_estimate = 4
          elsif todo_id=='2'
-                
                @todo_description ="Eat Snacks"
                @todo_pomodoro_estimate = 3
-             
+         elsif todo_id=='3'
+               @todo_description ="Get Supplies"
+               @todo_pomodoro_estimate = 2
+        elsif todo_id=='4'
+               @todo_description ="Order food for saturday and sunday"
+               @todo_pomodoro_estimate = 5
+        elsif todo_id=='5'
+               @todo_description ="Check Pre-Work Assighnments"
+               @todo_pomodoro_estimate = 4
         end
     end
 end
